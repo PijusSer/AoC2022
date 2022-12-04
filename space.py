9dbe8b8f840a7b842700d5bf6f8ec3a1806ccf9b
PijusSer <78708550+PijusSer@users.noreply.github.com>
2022-12-04 15:39:06 +0200

    Add files via upload

--- space.py
+++ space.py
@@ -1,10 +1,8 @@
-
-
 space = open(r'C:\Users\Pijus\Desktop\ipython\1204\input.txt', 'r')
-
 
 pairs = []
 s_l = []
+contain_count = 0
 overlap_count = 0
 
 for line in space:
@@ -12,7 +10,14 @@ for line in space:
     pairs = line.split(',')
     for sections in pairs:
         s_l += sections.split('-')
-    if set((range(int(s_l[0]), int(s_l[1])))).issubset(range(int(s_l[2]), int(s_l[3]))) or set((range(int(s_l[2]), int(s_l[3])))).issubset(range(int(s_l[0]), int(s_l[1]))):
+    a = int(s_l[0])
+    b = int(s_l[1])
+    c = int(s_l[2])
+    d = int(s_l[3])
+    if set(range(a, b+1)).issubset(range(c, d+1)) or set(range(c, d+1)).issubset(range(a, b+1)):
+        contain_count += 1
+    if not set(range(a, b+1)).isdisjoint(set(range(c, d+1))):
         overlap_count += 1
     s_l = []
+print (contain_count)
 print (overlap_count)
